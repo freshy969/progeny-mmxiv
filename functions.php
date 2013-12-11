019c3094a152e5deccc21f528c03d46fa1205591
Luke McDonald <thelukemcdonald@gmail.com>
2013-12-11 13:19:33 -0600

    Initial committ. Rough start.

---
+++ functions.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Activate the settings meta box on record and video archives.
+ */
+add_action( 'add_audiotheme_archive_settings_meta_box_audiotheme_record', '__return_true' );
+add_action( 'add_audiotheme_archive_settings_meta_box_audiotheme_video', '__return_true' );
+
+
+/**
+ * Activate default archive setting fields.
+ *
+ * @param array $fields List of default fields to activate.
+ * @param string $post_type Post type archive.
+ * @return array
+ */
+function twentyfourteen_audiotheme_archive_settings_fields( $fields, $post_type ) {
+	if ( ! in_array( $post_type, array( 'audiotheme_gig', 'audiotheme_record', 'audiotheme_video' ) ) ) {
+		return $fields;
+	}
+
+	$fields['columns'] = array(
+		'choices' => range( 1, 3 ),
+		'default' => 2,
+	);
+
+	$fields['posts_per_archive_page'] = true;
+
+	return $fields;
+}
+add_filter( 'audiotheme_archive_settings_fields', 'twentyfourteen_audiotheme_archive_settings_fields', 10, 2 );
+
+
+/**
+ * Before AudioTheme Main Content
+ */
+function twentyfourteen_audiotheme_before_main_content() {
+	echo '<div id="main-content" class="main-content">';
+	echo '<div id="primary" class="content-area">';
+	echo '<div id="content" class="site-content" role="main">';
+}
+add_action( 'audiotheme_before_main_content', 'twentyfourteen_audiotheme_before_main_content' );
+
+
+/**
+ * After AudioTheme Main Content
+ */
+function twentyfourteen_audiotheme_after_main_content() {
+	echo '</div><!-- #content -->';
+	echo '</div><!-- #primary -->';
+	echo '</div><!-- #main-content -->';
+	get_sidebar( 'content' );
+	get_sidebar();
+}
+add_action( 'audiotheme_after_main_content', 'twentyfourteen_audiotheme_after_main_content' );
+
+
+/**
+ * Register support for AudioTheme features.
+ */
+function nowell_audiotheme_setup() {
+	// Add thumbnail support to archive pages
+	//add_post_type_support( 'audiotheme_archive', 'thumbnail' );
+
+	// Add support for AudioTheme widgets
+	add_theme_support( 'audiotheme-widgets', array(
+		'recent-posts',
+		'record',
+		'track',
+		'upcoming-gigs',
+		'video'
+	) );
+}
+add_action( 'after_setup_theme', 'nowell_audiotheme_setup' );
