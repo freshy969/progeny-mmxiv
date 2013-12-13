b2abe9f5ba2c7231e0dbaed59129fe7db9901c7d
Luke McDonald <thelukemcdonald@gmail.com>
2013-12-13 15:13:22 -0600

    Update prefix

--- functions.php
+++ functions.php
@@ -1,5 +1,43 @@
 <?php
+/**
+ * Register support for AudioTheme features.
+ */
+function twentyfourteen_audiotheme_setup() {
+	// Add support for AudioTheme widgets
+	add_theme_support( 'audiotheme-widgets', array(
+		'record',
+		'track',
+		'upcoming-gigs',
+		'video'
+	) );
+}
+add_action( 'after_setup_theme', 'twentyfourteen_audiotheme_setup' );
 
+/**
+ * Before AudioTheme Main Content
+ */
+function twentyfourteen_audiotheme_before_main_content() {
+	echo '<div id="main-content" class="main-content">';
+	echo '<div id="primary" class="content-area">';
+	echo '<div id="content" class="site-content" role="main">';
+}
+add_action( 'audiotheme_before_main_content', 'twentyfourteen_audiotheme_before_main_content' );
+
+/**
+ * After AudioTheme Main Content
+ */
+function twentyfourteen_audiotheme_after_main_content() {
+	echo '</div><!-- #content -->';
+	echo '</div><!-- #primary -->';
+	echo '</div><!-- #main-content -->';
+	get_sidebar( 'content' );
+	get_sidebar();
+}
+add_action( 'audiotheme_after_main_content', 'twentyfourteen_audiotheme_after_main_content' );
+
+/**
+ * Adjust AudioTheme widget image sizes
+ */
 function twentyfourteen_audiotheme_widget_image_size( $size, $instance ) {
 	return array( 612, 612 ); // Sidebar width * 2
 }
@@ -8,15 +46,7 @@ add_filter( 'audiotheme_widget_track_image_size', 'twentyfourteen_audiotheme_wid
 add_filter( 'audiotheme_widget_video_image_size', 'twentyfourteen_audiotheme_widget_image_size', 20, 2 );
 
 /**
- * Extend the default WordPress post classes.
- *
- * Adds a post class to denote:
- * Non-password protected page with a post thumbnail.
- *
- * @since Twenty Fourteen 1.0
- *
- * @param array $classes A list of existing post class values.
- * @return array The filtered post class list.
+ * Extend the default AudioTheme post classes.
  */
 function twentyfourteen_audiotheme_post_classes( $classes ) {
 	global $post;
@@ -29,13 +59,11 @@ function twentyfourteen_audiotheme_post_classes( $classes ) {
 }
 add_filter( 'post_class', 'twentyfourteen_audiotheme_post_classes' );
 
-
 /**
  * Activate the settings meta box on record and video archives.
  */
 add_action( 'add_audiotheme_archive_settings_meta_box_audiotheme_record', '__return_true' );
 add_action( 'add_audiotheme_archive_settings_meta_box_audiotheme_video', '__return_true' );
-
 
 /**
  * Activate default archive setting fields.
@@ -59,45 +87,3 @@ function twentyfourteen_audiotheme_archive_settings_fields( $fields, $post_type 
 	return $fields;
 }
 add_filter( 'audiotheme_archive_settings_fields', 'twentyfourteen_audiotheme_archive_settings_fields', 10, 2 );
-
-
-/**
- * Before AudioTheme Main Content
- */
-function twentyfourteen_audiotheme_before_main_content() {
-	echo '<div id="main-content" class="main-content">';
-	echo '<div id="primary" class="content-area">';
-	echo '<div id="content" class="site-content" role="main">';
-}
-add_action( 'audiotheme_before_main_content', 'twentyfourteen_audiotheme_before_main_content' );
-
-
-/**
- * After AudioTheme Main Content
- */
-function twentyfourteen_audiotheme_after_main_content() {
-	echo '</div><!-- #content -->';
-	echo '</div><!-- #primary -->';
-	echo '</div><!-- #main-content -->';
-	get_sidebar( 'content' );
-	get_sidebar();
-}
-add_action( 'audiotheme_after_main_content', 'twentyfourteen_audiotheme_after_main_content' );
-
-
-/**
- * Register support for AudioTheme features.
- */
-function nowell_audiotheme_setup() {
-	// Add thumbnail support to archive pages
-	//add_post_type_support( 'audiotheme_archive', 'thumbnail' );
-
-	// Add support for AudioTheme widgets
-	add_theme_support( 'audiotheme-widgets', array(
-		'record',
-		'track',
-		'upcoming-gigs',
-		'video'
-	) );
-}
-add_action( 'after_setup_theme', 'nowell_audiotheme_setup' );
