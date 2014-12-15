9461fe7353ec292e129c24c8709e045cea3458d5
Luke McDonald <thelukemcdonald@gmail.com>
2014-12-15 11:20:22 -0600

    Add theme credits.

--- includes/hooks.php
+++ includes/hooks.php
@@ -22,3 +22,21 @@ function progeny_post_class( $classes ) {
 	return array_unique( $classes );
 }
 add_filter( 'post_class', 'progeny_post_class' );
+
+/**
+ * Theme credits text.
+ *
+ * @since 1.0.0
+ *
+ * @param string $text Text to display.
+ * @return string
+ */
+function progeny_credits() {
+	$text = sprintf( __( '%1$s WordPress theme by %2$s.', 'huesos' ),
+		'<a href="http://audiotheme.com/view/progeny-mmxiv/">Progeny MMXIV</a>',
+		'<a href="http://cedaro.com/">Cedaro</a>'
+	);
+
+	echo apply_filters( 'progeny_credits', $text );
+}
+add_action( 'twentyfourteen_credits', 'progeny_credits' );
